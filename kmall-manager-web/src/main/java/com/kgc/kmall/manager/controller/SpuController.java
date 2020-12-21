882723d1120ad20b3e5d769bd46778ea27005e6a
zhushuoyuan_glb <zhushuoyuan_glb@kgc.com>
2020-12-21 18:52:52 +0800

    commint

--- kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/SpuController.java
+++ kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/SpuController.java
@@ -3,10 +3,18 @@ package com.kgc.kmall.manager.controller;
 import com.kgc.kmall.bean.PmsBaseSaleAttr;
 import com.kgc.kmall.bean.PmsProductInfo;
 import com.kgc.kmall.service.SpuService;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.dubbo.config.annotation.Reference;
+import org.csource.common.MyException;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @CrossOrigin
@@ -14,6 +22,8 @@ import java.util.List;
 public class SpuController {
     @Reference
     SpuService spuService;
+    @Value("${fileServer.url}")
+    String fileUrl;
 
     @RequestMapping("/spuList")
     public List<PmsProductInfo> spuList(Long catalog3Id){
@@ -21,10 +31,30 @@ public class SpuController {
         return infoList;
     }
     @RequestMapping("/fileUpload")
-    public String fileUpload(@RequestParam("file")MultipartFile file){
+    public String fileUpload(@RequestParam("file")MultipartFile file) throws IOException, MyException {
         //文件上传
         //返回文件上传后的路径
-        return "https://m.360buyimg.com/babel/jfs/t5137/20/1794970752/352145/d56e4e94/591417dcN4fe5ef33.jpg";
+        String imgUrl=fileUrl;
+        if(file!=null){
+            System.out.println("multipartFile = " + file.getName()+"|"+file.getSize());
+
+            String configFile = this.getClass().getResource("/tracker.conf").getFile();
+            ClientGlobal.init(configFile);
+            TrackerClient trackerClient=new TrackerClient();
+            TrackerServer trackerServer=trackerClient.getTrackerServer();
+            StorageClient storageClient=new StorageClient(trackerServer,null);
+            String filename=    file.getOriginalFilename();
+            String extName = FilenameUtils.getExtension(filename);
+
+            String[] upload_file = storageClient.upload_file(file.getBytes(), extName, null);
+            imgUrl=fileUrl ;
+            for (int i = 0; i < upload_file.length; i++) {
+                String path = upload_file[i];
+                imgUrl+="/"+path;
+            }
+        }
+        System.out.println(imgUrl);
+        return imgUrl;
     }
     @RequestMapping("/baseSaleAttrList")
     public List<PmsBaseSaleAttr> baseSaleAttrList(){
--- kmall-manager-web/src/test/java/com/kgc/kmall/manager/KmallManagerWebApplicationTests.java
+++ kmall-manager-web/src/test/java/com/kgc/kmall/manager/KmallManagerWebApplicationTests.java
@@ -1,13 +1,34 @@
 package com.kgc.kmall.manager;
 
+import org.csource.common.MyException;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class KmallManagerWebApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws IOException, MyException {
+		String file = this.getClass().getResource("/tracker.conf").getFile();
+		ClientGlobal.init(file);
+		TrackerClient trackerClient=new TrackerClient();
+		TrackerServer trackerServer=trackerClient.getTrackerServer();
+		StorageClient storageClient=new StorageClient(trackerServer,null);
+		String orginalFilename="C:\\Users\\zhushuoyuan\\Desktop\\2.jpg";
+		String[] upload_file = storageClient.upload_file(orginalFilename, "jpg", null);
+		String path="http://192.168.88.12";
+		for (int i = 0; i < upload_file.length; i++) {
+			String s = upload_file[i];
+			System.out.println("s = " + s);
+			path+="/"+s;
+		}
+		System.out.println(path);
 	}
 
 }
